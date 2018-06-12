c2216440ac0ce4fb159ad72bc00c4b1fcce048ee
bradyxiao <bradyxiao@tencent.com>
2018-06-12 14:57:15 +0800

    test

--- flashligth/src/main/java/com/common/http/IDownloader.java
+++
@@ -1,9 +0,0 @@
-package com.common.http;
-
-public interface IDownloader {
-	
-	void updateDownloadProgress(final HttpDownloader.DownloadProgress dp);
-	void installApk(final String apkFilePath);
-	void onSDCardNotFound();
-	String getApkFilePath();
-}
